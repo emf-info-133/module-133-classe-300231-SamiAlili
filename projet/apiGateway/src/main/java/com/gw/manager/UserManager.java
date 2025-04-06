bf5d4947d5363108891789ae6e614d6e220f699d
SamiAlili <sami.alili@studentfr.ch>
2025-04-06 18:07:14 +0200

    ajout d'une variable d'environement pour le lien vers le rest1

--- projet/apiGateway/src/main/java/com/gw/manager/UserManager.java
+++ projet/apiGateway/src/main/java/com/gw/manager/UserManager.java
@@ -1,5 +1,16 @@
 package com.gw.manager;
 
+import org.springframework.web.client.RestTemplate;
+
 public class UserManager {
+
+    private static final String USER_SERVICE_URL = System.getenv().getOrDefault("REST1_SERVICE_URL",
+            "http://localhost:8081/rest1/");
+
+    private final RestTemplate restTemplate;
+
+    public UserManager(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
 }
