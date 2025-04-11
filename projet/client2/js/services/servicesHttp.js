2fb5fbc33c2f78c501cb35478fca02e37b0410dd
SamiAlili <sami.alili@studentfr.ch>
2025-04-11 14:59:42 +0200

    correction client2 servicehttp.js pour utiliser http plutôt que https

--- projet/client2/js/services/servicesHttp.js
+++ projet/client2/js/services/servicesHttp.js
@@ -5,134 +5,133 @@
  * @version 1.0 / 13-SEP-2013
  */
 
-var BASE_URL = "https://localhost:8080/gw/";
+var BASE_URL = "http://localhost:8080/gw/";
 
 function connect(username, passwd, successCallback, errorCallback) {
-  console.log("test");
-  $.ajax({
-    type: "POST",
-    dataType: "json",
-    url: BASE_URL + "login",
-    data: {
-      nom_utilisateur: username,
-      mdp: passwd
-    },
-    success: successCallback,
-    error: errorCallback
-  });
-
+    console.log("test");
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: BASE_URL + "login",
+        data: {
+            nom_utilisateur: username,
+            mdp: passwd,
+        },
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 /**
- * 
- * @param {*} username 
- * @param {*} passwd 
- * @param {*} successCallback 
- * @param {*} errorCallback 
+ *
+ * @param {*} username
+ * @param {*} passwd
+ * @param {*} successCallback
+ * @param {*} errorCallback
  */
 function signin(username, passwd, successCallback, errorCallback) {
-  $.ajax({
-    type: "POST",
-    dataType: "json",
-    url: BASE_URL + "loginManager.php",
-    data: 'action=signin&username=' + username + '&password=' + passwd,
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: BASE_URL + "loginManager.php",
+        data: "action=signin&username=" + username + "&password=" + passwd,
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 /**
- * 
- * @param {*} successCallback 
- * @param {*} errorCallback 
+ *
+ * @param {*} successCallback
+ * @param {*} errorCallback
  */
 function logout(successCallback, errorCallback) {
-  $.ajax({
-    type: "POST",
-    dataType: "json",
-    url: BASE_URL + "loginManager.php",
-    data: 'action=logout',
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: BASE_URL + "loginManager.php",
+        data: "action=logout",
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 //gestion équipes
 
 /**
- * 
- * @param {*} successCallback 
- * @param {*} errorCallback 
+ *
+ * @param {*} successCallback
+ * @param {*} errorCallback
  */
 function getEquipes(successCallback, errorCallback) {
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: BASE_URL + "equipeManager.php",
-    data: 'action=getEquipes',
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: BASE_URL + "equipeManager.php",
+        data: "action=getEquipes",
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 function ajouterEquipe(nomEquipe, successCallback, errorCallback) {
-  $.ajax({
-    type: "POST",
-    dataType: "json",
-    url: BASE_URL + "equipeManager.php",
-    data: 'action=ajouterEquipe&nom=' + nomEquipe,
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: BASE_URL + "equipeManager.php",
+        data: "action=ajouterEquipe&nom=" + nomEquipe,
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 function supprimerEquipe(pkEquipe, successCallback, errorCallback) {
-  $.ajax({
-    type: "DELETE",
-    dataType: "json",
-    url: BASE_URL + "equipeManager.php",
-    data: 'action=supprimerEquipe&pkEquipe=' + pkEquipe,
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "DELETE",
+        dataType: "json",
+        url: BASE_URL + "equipeManager.php",
+        data: "action=supprimerEquipe&pkEquipe=" + pkEquipe,
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 function getCompositionEquipe(pkEquipe, successCallback, errorCallback) {
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: BASE_URL + "equipeManager.php",
-    data: 'action=getCompositionEquipe&pkEquipe=' + pkEquipe,
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: BASE_URL + "equipeManager.php",
+        data: "action=getCompositionEquipe&pkEquipe=" + pkEquipe,
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 function sauvegarderEquipe(pkEquipe, joueurs, successCallback, errorCallback) {
-  //    data: 'action=sauvegarderEquipe&pkEquipe=' + pkEquipe + '&pksJoueurs=' + joueurs,
+    //    data: 'action=sauvegarderEquipe&pkEquipe=' + pkEquipe + '&pksJoueurs=' + joueurs,
 
-  $.ajax({
-    type: "PUT",
-    dataType: "json",
-    url: BASE_URL + "equipeManager.php",
-    data: {
-      action: "sauvegarderEquipe",
-      pkEquipe: pkEquipe,
-      pksJoueurs: joueurs,
-    },
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "PUT",
+        dataType: "json",
+        url: BASE_URL + "equipeManager.php",
+        data: {
+            action: "sauvegarderEquipe",
+            pkEquipe: pkEquipe,
+            pksJoueurs: joueurs,
+        },
+        success: successCallback,
+        error: errorCallback,
+    });
 }
 
 //gestion des joueurs
 function getAllJoueurs(successCallback, errorCallback) {
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: BASE_URL + "joueurManager.php",
-    data: 'action=getAllJoueurs',
-    success: successCallback,
-    error: errorCallback
-  });
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: BASE_URL + "joueurManager.php",
+        data: "action=getAllJoueurs",
+        success: successCallback,
+        error: errorCallback,
+    });
 }
